a43123b184eae436734bd3dcf52f0aa41bbbc91f
Sergey Savchuk <ssavchuk@AMSssavchukWWGTFM.local>
2018-02-26 01:23:16 +0100

    Login test for Python is done.

--- v_python/test_google_search.py
+++ v_python/test_google_search.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 @pytest.fixture
@@ -11,9 +12,20 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
+test_url = 'http://localhost/litecart/admin/'
+username = 'admin'
+password = 'admin'
+
+def update_input(wd, value, locator):
+    input = wd.find_element_by_css_selector(locator)
+    input.click()
+    input.clear()
+    input.send_keys(value)
+
 
 def test_google_search(driver):
-    driver.get("http://www.google.com/ncr")
-    driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnK").click()
-    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Google Search"))
+    driver.get(test_url)
+    update_input(driver, username, 'input[name="username"]')
+    update_input(driver, password, 'input[name="password"]')
+    driver.find_element_by_name('login').click()
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#box-apps-menu-wrapper')))
